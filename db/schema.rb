44df140cf37cdec6ae7fa8d452bdb66ec1b901b1
Indira Williams <indirajamurath@gmail.com>
2018-07-07 13:52:38 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114213039) do
+ActiveRecord::Schema.define(version: 3) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "content"
